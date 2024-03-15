c33a43ac6c28e00df9e2bc44a98cc6fd182c1732
Vladimir <mr.vorob@bk.ru>
2024-03-15 21:55:29 +0600

    add: homework-3

---
+++ script.js
@@ -0,0 +1,27 @@
+const fizzbuzz = () => {
+  let i = 1n;
+
+  let next = () => {
+    let result = i;
+
+    if (i % 3n == 0n && i % 5n == 0n) {
+      result = 'FizzBuzz';
+    } else if (i % 3n == 0n) {
+      result = 'Fizz';
+    } else if (i % 5n == 0) {
+      result = 'Buzz';
+    }
+
+    i++;
+
+    return result;
+  };
+
+  return { next };
+};
+
+const myFizzBuzz = fizzbuzz();
+
+for (let i = 0n; i < 50n; i++) {
+  console.log(myFizzBuzz.next());
+}
