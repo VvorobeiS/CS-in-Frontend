27fbe9930e4d7e3da86debafbd0133ea51f4aa07
Vladimir <mr.vorob@bk.ru>
2024-04-01 18:12:24 +0600

    update: master

--- dist/helpers.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.calcNumberLength = void 0;
-const convertToBcd = (num, shift) => { };
-const calcNumberLength = (num) => Math.floor(Math.log10(Math.abs(num)) + 1);
-exports.calcNumberLength = calcNumberLength;
-//# sourceMappingURL=helpers.js.map
