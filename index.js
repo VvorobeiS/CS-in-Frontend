9f17f35a7f694199e1038ca34ce4880f7c504603
Vladimir <mr.vorob@bk.ru>
2024-04-18 18:09:40 +0600

    update: homework-8

--- index.js
+++ index.js
@@ -31,8 +31,11 @@ class Vector {
   }
 
   *values() {
-    let idx = 0;
-    let pointer = this.#array[idx];
+    let i = 0;
+
+    while (i < this.#length) {
+      yield this.#array[i++];
+    }
   }
 
   shrinkToFit() {
@@ -56,13 +59,70 @@ class Vector {
 }
 
 const vec = new Vector(Int32Array, { capacity: 4 });
+const i = vec.values();
 
 vec.push(1);
 vec.push(2);
 vec.push(3);
 vec.push(4);
 vec.push(5);
-vec.pop();
-vec.shrinkToFit();
+vec.push(5);
 
-// console.log(vec.capacity, vec.length, vec.buffer);
+console.log(i.next());
+console.log(i.next());
+console.log(i.next());
+console.log(i.next());
+
+class Matrix3D {
+  #capacity;
+  #array;
+  #length;
+  #x;
+  #y;
+  #z;
+
+  constructor(TypedArray, x, y, z) {
+    this.#capacity = x * y * z;
+    this.#x = x;
+    this.#y = y;
+    this.#z = z;
+    this.#array = new TypedArray(this.#capacity);
+    this.#length = 0;
+  }
+
+  calcIdx(x, y, z) {
+    let lengthX = this.#capacity / this.#x;
+    let lengthY = lengthX / this.#y;
+    let shiftX = lengthX * x;
+    let shiftY = lengthY * y;
+
+    return shiftX + shiftY + z;
+  }
+
+  set(x, y, z, value) {
+    this.#array[this.calcIdx(x, y, z)] = value;
+    this.#length++;
+  }
+
+  *values() {
+    let i = 0;
+
+    while (i < this.#length) {
+      yield this.#array[i++];
+    }
+  }
+}
+
+const newMatrix3D = new Matrix3D(Int32Array, 2, 2, 2);
+
+newMatrix3D.set(0, 0, 0, 1);
+newMatrix3D.set(0, 0, 1, 2);
+newMatrix3D.set(0, 1, 0, 3);
+newMatrix3D.set(0, 1, 1, 4);
+
+newMatrix3D.set(1, 0, 0, 5);
+newMatrix3D.set(1, 0, 1, 6);
+newMatrix3D.set(1, 1, 0, 7);
+newMatrix3D.set(1, 1, 1, 8);
+
+console.log(Array.from(newMatrix3D.values()));
